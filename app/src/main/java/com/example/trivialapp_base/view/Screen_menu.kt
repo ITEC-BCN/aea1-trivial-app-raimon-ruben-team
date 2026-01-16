1dfc0d70bd405d57032c127b9bcb09c5d9c0d5a0
Ruben Gimenez <ruben.gimenez.7e9@itb.cat>
2026-01-16 09:51:34 +0100

    Conflicts Resolved

--- app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
@@ -48,7 +48,7 @@ fun MenuScreen(navController: NavController, viewModel: GameViewModel) {
                 .clickable { navController.navigate(Routes.Game.route) })
     }
 
-    @Composable
+
     fun MenuDificultad() {
         var expanded by remember { mutableStateOf(false) }
         var dificultadSeleccionada by remember { mutableStateOf("Selecciona dificultad") }
